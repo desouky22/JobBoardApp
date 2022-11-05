f98bc637ccbc1b76e89dd5bc4d3ac6482eeeda7d
Omar Desouky <omaressamdesouky2274@gmail.com>
2022-11-05 07:39:32 +0200

    add templates and views

--- job/views.py
+++ job/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Job, Category
+
 
 def job_list(request, *args, **kwargs):
-    pass
+    queryset = Job.objects.all()
+    context = {"jobs": list(queryset)}
+    return render(request, "job/job_list.html", context)
 
 
 def job_detail(request, *args, **kwargs):
-    pass
+    try:
+        job = Job.objects.get(pk=kwargs["pk"])
+        context = {"job": job}
+    except Job.DoesNotExist:
+        context = {"job": "NOT FOUND"}
 
+    return render(request, "job/job_detail.html", context)
